8b385e189eef9cbe30c10078dcb3606dd4070b9f
adityaferdi77 <adityaferdi77@gmail.com>
2023-02-07 12:04:56 +0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,19 @@
         <div class="account-box">
           <div class="login-header">
         <div class="card shadow">
-          <div class="card-body bg-black text-white"><center>
-              Silahkan Login Untuk Berpartisipasi
-          </center></div>
+          <div class="card-body bg-dark text-white">
+              {{-- Silahkan Login Untuk Berpartisipasi --}}
+              <marquee behavior="" direction="">
+                <h4 class="fw-bold">
+                    <span class="text-brand ">Pengaduan masyarakat adalah penyampaian keluhan oleh masyarakat kepada pemerintah atas pelayanan yang tidak sesuai dengan standar pelayanan, atau pengabaian kewajiban dan/atau pelanggaran larangan.</span>
+                </h4>
+            </marquee>
+          </div>
           <div class="card-body mx-auto">
               <a href="{{url('/masyarakat/login')}}" class="btn btn-danger text-white">Login Masyarakat</a>
               <a href="{{url('/petugas/login')}}" class="btn btn-info text-white">Login Petugas</a>
+            </div>
+
           </div>
         </div>
       </div>
