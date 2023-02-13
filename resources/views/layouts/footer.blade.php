c5d6a2595a0cc668cf4ebeb23d236054b56c59b7
adityaferdi77 <adityaferdi77@gmail.com>
2023-02-13 09:49:39 +0800

    Dashboard Masyarakat & Admin

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+
+<footer>
+    <div class="row text-center">
+    <p>Copyright &copy; {{date('Y')}} Pengaduan Masyarakat created by Kelompok 3 <span style="color: #f57d00;">&#10084;</span></p>
+</footer>
